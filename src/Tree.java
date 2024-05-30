6ca4fc6b6310fed5c271192ae6fb904c45aac645
Nikoo Ghafouri <nikooghafouri@nikoos-air.home>
2024-05-30 14:30:16 -0400

    added constructor, is_Empty, and Length methods

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,34 @@
-public class Tree {
+import java.util.List;
+import java.util.Optional;
+
+public class Tree<T> {
+
+    private T root;
+    private List<Tree<T>> subtrees;
+
+    private Tree(T root, List<Tree<T>> subtrees) {
+        this.root = root;
+        this.subtrees = subtrees;
+    }
+
+    private boolean isEmpty() {
+
+        boolean is_empty = this.root == null;
+        return is_empty;
+
+    }
+
+    private int Length() {
+        if (this.isEmpty()) {
+            return 0;
+        }
+        else {
+            int size = 1;
+            for (Tree<T> subtree : this.subtrees) {
+                size += subtree.Length();
+            }
+            return size;
+        }
+    }
+
 }
